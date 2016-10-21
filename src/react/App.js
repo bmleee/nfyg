ad9101c9a26dc44be50315f0badd778369dd8e98
4000D <dkdkajej@gmail.com>
2016-10-22 06:48:54 +0900

    React/App.js Commented

--- src/react/App.js
+++ src/react/App.js
@@ -13,6 +13,10 @@ import routes from './routes';
 const store = configureStore(browserHistory, window.__initialState__);
 const history = syncHistoryWithStore(browserHistory, store);
 
+/**
+ * Wrap top-level App component inside a react-reduc Provider
+ * 	to connect component tree to React store
+ */
 render((
 	<Provider store={store}>
 		<Router history={history} routes={routes} />
