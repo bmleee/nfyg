f711b18722c355a33a1a7a98ec708f5c27ff979d
4000D <dkdkajej@gmail.com>
2016-10-31 14:19:01 +0900

    custom throttle function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,3 +3,31 @@ export const canUseDOM = !!(
   window.document &&
   window.document.createElement
 )
+
+export const throttle = (f, wait) => {
+	let context, args, preArgs, argsChanged, result;
+	let previous = 0;
+
+	return function() {
+		let now, remaining;
+
+		if(wait) {
+			now = Date.now();
+			remaining = wait - (now - previous);
+		}
+
+		context = this;
+		args = arguments;
+		argsChanged = JSON.stringify(args) != JSON.stringify(preArgs);
+		preArgs = Object.assign({}, args);
+
+		if(argsChanged || wait && (remaining <= 0 || remaining > wait)) {
+			if(wait) previous = now;
+
+			result = f.apply(context, args);
+			context = args = null;
+
+			return result;
+		}
+	};
+}
