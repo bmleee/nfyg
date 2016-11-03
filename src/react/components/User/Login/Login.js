ac60918753007071c0d12a5528772e58ecaa6d18
4000D <dkdkajej@gmail.com>
2016-11-03 16:43:50 +0900

    fix props naming convention

--- src/react/components/User/Login/Login.js
+++ src/react/components/User/Login/Login.js
@@ -18,14 +18,14 @@ class Login extends Component {
 		return (
 			<div className="user-login">
 				<button className="fb-login-btn" onClick={this._onFacebookClick}>
-				<FontAwesome className="fb-login-icon" name='facebook' size='lg' />
-				페이스북으로 로그인
+					<FontAwesome className="fb-login-icon" name='facebook' size='lg' />
+					페이스북으로 로그인
 				</button>
 				<div>
 					<input type="text" className="user-login-id" name="user_id" placeholder="이메일" ref="user_id"/>
 				</div>
 				<div>
-					<input type="password" className="user-login-password" name="user_password" placeholder="비밀번호" ref="user_password" autocomplete="off" />
+					<input type="password" className="user-login-password" name="user_password" placeholder="비밀번호" ref="user_password" autoComplete="off" />
 				</div>
 				<button className="login-btn" onClick={this._onClick}>LOG IN</button>
 			</div>
