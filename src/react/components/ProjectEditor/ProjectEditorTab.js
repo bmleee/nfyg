8ce0c16e9ef63b91c53add646594777bb62c801d
7pictures <pjh@7pictures.co.kr>
2016-11-20 13:58:08 +0000

    styling

--- src/react/components/ProjectEditor/ProjectEditorTab.js
+++ src/react/components/ProjectEditor/ProjectEditorTab.js
@@ -1,10 +1,10 @@
 import { Link } from 'react-router'
 
 const ProjectEditorTab = () => (
-	<div>
-		<Link to="/project-editor/abstract"><button>Abstract</button></Link>
-		<Link to="/project-editor/funding"><button>Funding</button></Link>
-		<Link to="/project-editor/overview"><button>Overview</button></Link>
+	<div className="project-editor-tab">
+		<Link className="project-editor-tab-button" to="/project-editor/abstract"><button>프로젝트 개요</button></Link>
+		<Link to="/project-editor/funding"><button className="project-editor-tab-button">펀딩 및 리워드</button></Link>
+		<Link to="/project-editor/overview"><button className="project-editor-tab-button">프로젝트 소개</button></Link>
 	</div>
 )
 
