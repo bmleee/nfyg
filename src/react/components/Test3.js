bf884b81b8791976cadf6cfcd38cff9b392a0b38
4000d <dkdkajej@gmail.com>
2016-11-29 18:04:55 +0900

    test...

--- src/react/components/Test3.js
+++ src/react/components/Test3.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Editor, { EditorState } from 'draft-js'
+import { EditorState, ContentState, convertFromRaw } from 'draft-js'
+import SevenEditor, { Viewer } from './DraftEditor/SevenEditor'
 
 // import AddImageEditor from './DraftEditor/AddImageEditor'
 // const t = () => <AddImageEditor />
@@ -7,7 +8,6 @@ import Editor, { EditorState } from 'draft-js'
 // import DraftEditor from './DraftEditor'
 // const t = () => <DraftEditor />
 
-import SevenEditor from './DraftEditor/SevenEditor'
 
 // const t = () => <DraftEditor />
 //
@@ -15,23 +15,33 @@ import SevenEditor from './DraftEditor/SevenEditor'
 //
 // export default t
 
+
+const raw = JSON.parse('{"entityMap":{"0":{"type":"image","mutability":"IMMUTABLE","data":{"src":"/uploads/user_default.png"}},"1":{"type":"image","mutability":"IMMUTABLE","data":{"src":"/uploads/favicon.ico","alignment":"center"}}},"blocks":[{"key":"33h3v","text":"sadfsdf","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"f9s8u","text":"asdfasfa","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"bstai","text":"asdf","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"10hso","text":" ","type":"atomic","depth":0,"inlineStyleRanges":[],"entityRanges":[{"offset":0,"length":1,"key":0}],"data":{}},{"key":"cor9t","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}},{"key":"c26gs","text":" ","type":"atomic","depth":0,"inlineStyleRanges":[],"entityRanges":[{"offset":0,"length":1,"key":1}],"data":{}},{"key":"d5hdp","text":"","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}]}')
+
 export default class T extends Component {
 	state = {
-		editorState: EditorState.createEmpty()
+		raw: ''
 	}
 
-	_onEditorChange = (editorState) => this.setState({ editorState })
+	_onEditorChange = (raw) => {
+		if(raw) {
+			console.log('Test3._onEditorChange.raw', raw);
+			this.setState({ raw })
+		}
+	}
 
 	render() {
-		console.log('this.state.editorState', this.state.editorState);
+		let {
+			raw
+		} = this.state;
+
 		return (
 			<div>
 				<SevenEditor onChange={this._onEditorChange} />
 
 				<h3>Read Only</h3>
-				<Editor
-					editorState={this.state.editorState}
-					readOnly={true}
+				<Viewer
+					raw={raw}
 				/>
 			</div>
 		)
