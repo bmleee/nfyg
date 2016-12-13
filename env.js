dc8bd191575f368d098956b237b4131e87f3b2f7
4000d <dkdkajej@gmail.com>
2016-12-13 16:25:43 +0900

    update db config

--- env.js
+++ env.js
@@ -2,11 +2,12 @@ var dbconfig = {
 	dbname: 'seven_pictures',
 	dbuser: '7pictures',
 	dbpassword: '7pictures',
+	dbhost: 'ds133348.mlab.com:33348'
 }
 
 
 module.exports = {
 	DEV_PORT: 3002,
 	EXPRESS_PORT: process.env.PORT || 3100,
-	MONGODB_URL: `mongodb://${dbconfig.dbuser}:${dbconfig.dbpassword}@ds041643.mlab.com:41643/seven_pictures`,
+	MONGODB_URL: `mongodb://${dbconfig.dbuser}:${dbconfig.dbpassword}@${dbconfig.dbhost}/${dbconfig.dbname}`,
 };
