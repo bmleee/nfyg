cd51eadec14848433d6daa3bc4427ccc59183c42
7pictures <pjh@7pictures.co.kr>
2016-12-29 17:08:53 +0900

    styling~

--- src/react/components/ProductDetail/components/ProductTab.js
+++ src/react/components/ProductDetail/components/ProductTab.js
@@ -2,8 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import cx from 'classnames'
 
-const buttonClassName = 'project-tab-button';
-const appliedClassName = 'project-tab-button-clicked';
+const buttonClassName = 'product-tab-button';
+const appliedClassName = 'product-tab-button-clicked';
 
 class ProductTab extends Component {
 	render() {
@@ -19,18 +19,18 @@ class ProductTab extends Component {
 		const isQnA = !!document.URL.match(/products\/.+\/qna/)
 		const isOverview = !(isPost || isRanking || isQnA)
 
-		const overviewClassName = cx({'project-tab-button': true, 'project-tab-button-clicked': isOverview})
-		const postClassName = cx({'project-tab-button': true, 'project-tab-button-clicked': isPost})
-		const rankClassName = cx({'project-tab-button': true, 'project-tab-button-clicked': isRanking})
-		const qnaClassName = cx({'project-tab-button': true, 'project-tab-button-clicked': isQnA})
+		const overviewClassName = cx({'product-tab-button': true, 'product-tab-button-clicked': isOverview})
+		const postClassName = cx({'product-tab-button': true, 'product-tab-button-clicked': isPost})
+		const rankClassName = cx({'product-tab-button': true, 'product-tab-button-clicked': isRanking})
+		const qnaClassName = cx({'product-tab-button': true, 'product-tab-button-clicked': isQnA})
 
 		// TODO: Apply :product_name
 		return (
 			<div className="project-detail-tab">
 				<Link to={`/products/${productName}`}><button className={overviewClassName}>소 개</button></Link>
-				<Link to={`/products/${productName}/post`}><button className={postClassName}>소 식<img className="tab-red-dot" src="/assets/images/red-dot.png" width={4} height={4} /></button></Link>
-				<Link to={`/products/${productName}/ranking`}><button className={rankClassName}>응 원</button></Link>
-				<Link to={`/products/${productName}/qna`}><button className={qnaClassName}>문 의</button></Link>
+				<Link to={`/products/${productName}/post`}><button className={postClassName}>FAQ</button></Link>
+				{/* <Link to={`/products/${productName}/ranking`}><button className={rankClassName}>응 원</button></Link> */}
+				<Link to={`/products/${productName}/qna`}><button className={qnaClassName}>댓 글</button></Link>
 				{/* <button className="project-support-button">후원하기</button> */}
 			</div>
 			)
