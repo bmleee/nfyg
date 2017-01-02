9bd3dfd74ea4c15316f3afc0600e4efd2c03572a
7pictures <pjh@7pictures.co.kr>
2017-01-02 20:25:03 +0900

    styling~

---
+++ src/react/components/Home/components/PresentProductList.js
@@ -0,0 +1,109 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import Progress from 'react-progressbar';
+
+ 
+
+/**
+ * required state
+{
+ 	// desktop
+	current_page, :: thunbmail list
+
+	// mobile
+	current_product_index :: image-gallery
+}
+ */
+
+class PresentProductList extends Component {
+	constructor() {
+		super(...arguments);
+
+		this.state = {
+			numProducts: 0,
+			count: 4,
+			windowSize: 4,
+		}
+	}
+
+	// TODO: Async expand list (ajax request to more products)
+	/*
+		async 버전:
+			GET /products?from=0, count=3
+				- productList 일부 반환
+				- more_products: Boolean 으로 '더 많은 프로젝트 보기' 버튼 유지 / 삭제
+	*/
+	expandList() {
+		this.setState({
+			count: this.state.count + this.state.windowSize,
+		})
+	}
+	
+	componentDidMount() {
+		this.setState({
+			numProducts: this.props.products.length
+		})
+	}
+
+	render() {
+		let { products, } = this.props;
+
+		let productList = products.map(
+			({
+				imgSrc,
+				creator,
+				title,
+				currentMoney,
+				targetMoney,
+				numDirectSupports,
+				numIndirectSupports,
+				remainingDays,
+				link,
+				postIntro,
+			}, index) => (
+				<div className="present-project-list-item-container">
+					<div className="present-project-list-item" key={index}>
+						<Link to={link}> {/* TODO: include :product_name */}
+							<div className="pr-thumbnail">
+								<div className="ex-centered">
+									<img className="home-exhibition-image" src={imgSrc} />
+								</div>
+							</div>
+						</Link>
+						<div className="present-project-list-item-caption">
+							<Link to={link}><h3 className="project-list-title">{title}</h3></Link>
+							<h5>{postIntro}</h5>
+							<div className="product-purchase-num"><p>000명</p>주문중</div>
+						    {/* 직접후원 {numDirectSupports}명 | 간접후원 {numIndirectSupports}명 */}
+						</div>
+					</div>
+				</div>
+			)
+		);
+
+		return (
+			<div className="present-project-list">
+				<div className="present-project-list-container">
+					{ productList.slice(0, this.state.count) }
+				</div>
+				<div className="present-more-project">
+					{
+						this.state.numProducts > 4 && this.state.numProducts > this.state.count
+							? <button className="present-more-button" onClick={this.expandList.bind(this)}> VIEW MORE</button>
+							: null
+					}
+				</div>
+			</div>
+
+		)
+	}
+}
+
+
+PresentProductList.propTypes = {
+	products: PropTypes.arrayOf(PropTypes.shape({
+
+	})),
+}
+
+export default PresentProductList;
--- src/react/components/ProductDetail/components/Post/Post.js
+++ src/react/components/ProductDetail/components/Post/Post.js
@@ -5,8 +5,7 @@ import {
 } from './';
 
 import {date2string} from '~/src/react/lib/utils'
-import Collapse, { Panel } from 'rc-collapse';
-
+import Collapsible from 'react-collapsible';
 
 class Post extends Component {
 
@@ -51,11 +50,45 @@ class Post extends Component {
 				{ opened ? <PostComments comments={comments} postLikes={likes} /> : null }
 			</div>
 		))
+		
+		
 
 		return (
 			<div className="project-detail-post">
-				<div className="project-detail-post-container">
-		
+				<div className="product-faq-container">
+				<Collapsible trigger="제품 구매 이력은 어디서 확인할 수 있나요?" transitionTime="0" open="true">
+    			 <p>-This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>-This is the collapsible content. It can be any element or React component you like.</p>
+    			</Collapsible>
+    			<Collapsible trigger="배송지를 변경하고 싶습니다." transitionTime="0">
+    			 <p>#This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>#This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>#This is the collapsible content. It can be any element or React component you like.</p>
+    			</Collapsible>
+    			<Collapsible trigger="언제 배송되나요?" transitionTime="0">
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			</Collapsible>
+    			<Collapsible trigger="결제 취소가 가능한가요?" transitionTime="0">
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			</Collapsible>
+    			<Collapsible trigger="옵션을 변경하고 싶습니다. 가능한가요?" transitionTime="0">
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			</Collapsible>
+    			<Collapsible trigger="교환이나 반품/환불이 가능한가요?" transitionTime="0">
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			 <p>&This is the collapsible content. It can be any element or React component you like.</p>
+    			</Collapsible>
 				</div>
 			</div>
 			)
