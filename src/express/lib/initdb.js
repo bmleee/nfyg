6df96146b307ee793e54ae6c308a67d0ca419ca8
4000d <dkdkajej@gmail.com>
2017-01-10 20:37:56 +0900

    refactor : apply dynamic module import  - npm run initdb sometimes faild because dropDatabase() is called after mongoose model scheme is established.  - make sure drop database before model import

--- src/express/lib/initdb.js
+++ src/express/lib/initdb.js
@@ -1,28 +1,6 @@
 'use strict'
 import mongoose from 'mongoose'
-
-import UserModel from '../models/user'
-import ProjectModel from '../models/project'
-import ProductModel from '../models/product'
-import SponsorModel from '../models/sponsor'
-import PostModel from '../models/post'
-import MagazineModel from '../models/magazine'
-import ExhibitionModel from '../models/exhibition'
-import QnAModel from '../models/qna'
-import AddressModel from '../models/address'
-import PurchaseModel from '../models/purchase'
-
-import Users from './initdb.users'
-import Sponsors from './initdb.sponsors'
-import Projects from './initdb.projects'
-import Products from './initdb.products'
-import Posts from './initdb.posts'
-import Magazines from './initdb.magazines'
-import Exhibitions from './initdb.exhibitions'
-import QnA from './initdb.qnas'
-import Actions from './initdb.user-action'
-
-import init from './initdb.helper'
+import db from './db'
 
 const test = async () => {
 	try {
@@ -50,34 +28,35 @@ mongoose.connection.on('open', async function(){
 })
 
 async function main() {
+	const init = require('./initdb.helper').default
 	try {
 		console.log('init db start');
 		let t1 = Date.now(), t2 = Date.now();
 
 		await Promise.all([
-			Users(),
-			Sponsors()
+			require('./initdb.users').default(),
+			require('./initdb.sponsors').default(),
 		])
 		console.log(`${((t2 = Date.now()) - t1) / 1000}sec`);
 
 		await init()
 		await Promise.all([
-			Magazines(),
-			Exhibitions(),
-			Projects(),
-			Products(),
+			require('./initdb.magazines').default(),
+			require('./initdb.exhibitions').default(),
+			require('./initdb.projects').default(),
+			require('./initdb.products').default(),
 		])
 		console.log(`${((t2 = Date.now()) - t1) / 1000}sec`);
 
 		await init()
 		await Promise.all([
-			Posts(),
+			require('./initdb.posts').default(),
 		])
 
-		await QnA()
+		await require('./initdb.qnas').default()
 		console.log(`${((t2 = Date.now()) - t1) / 1000}sec`);
 
-		await Actions()
+		await require('./initdb.user-action').default()
 		console.log(`${((t2 = Date.now()) - t1) / 1000}sec`);
 
 		console.log('init db finished');
@@ -85,9 +64,10 @@ async function main() {
 
 		await test()
 
-		await exit(0)
+		// await process.exit(0)
 	} catch (e) {
 		console.error(e);
+		process.exit(-1)
 	}
 
 }
