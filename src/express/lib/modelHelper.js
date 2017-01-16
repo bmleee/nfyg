9e792185bf27f68d1ec827f7d92e63cf1ec2bb57
4000d <dkdkajej@gmail.com>
2017-01-16 23:04:22 +0900

    CRUD helper

--- src/express/lib/modelHelper.js
+++ src/express/lib/modelHelper.js
@@ -1,4 +1,5 @@
 import QnAModel from '../models/qna'
+import PostModel from '../models/post'
 
 export async function createQnA({project, product, title, text, user}) {
   let body = {
@@ -22,6 +23,7 @@ export async function createQnA({project, product, title, text, user}) {
   return await QnAModel.create(body)
 }
 
+// DEPRECIATED. refer QnAModel.methods.commentedByUser
 export async function createCommentOnQnA({_id, text, user}) {
   let body = {
     author: {
@@ -34,6 +36,46 @@ export async function createCommentOnQnA({_id, text, user}) {
   }
 
   return await QnAModel.update(
+    { _id: _id },
+    { $addToSet: { comments: body } }
+  )
+}
+
+// DEPRECIATED. refer PostModel.methods.commentedByUser
+export async function createPost({project, product, user, title, content, thresholdMoney, isDirectSupport}) {
+  let body = {
+    author: {
+      name: user.display_name,
+      iconSrc: user.image,
+      user: user._id,
+    },
+    project: project,
+    product: product,
+    abstract: {
+      isDirectSupport,
+      title,
+      thresholdMoney,
+      likes: [],
+    },
+    content,
+    comments: [],
+  }
+
+  return await PostModel.create(body)
+}
+
+export async function createCommentOnPost({_id, text, user}) {
+  let body = {
+    author: {
+      name: user.display_name,
+      iconSrc: user.image,
+      user: user._id,
+    },
+    text,
+    likes: [],
+  }
+
+  return await PostModel.update(
     { _id: _id },
     { $addToSet: { comments: body } }
   )
