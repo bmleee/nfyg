ee1b2229ceca9e6b0f18fc1e4b94ceef2439645a
4000d <dkdkajej@gmail.com>
2017-01-20 07:49:53 +0900

    User Editor first draft

---
+++ src/react/components/UserEditor/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+export default class UserEditor extends Component {
+  state = {
+    user: {
+
+    }
+  }
+
+  async componentDidMount() {
+
+  }
+  render() {
+    const {
+      users // raw project model
+    } = this.props
+
+    return (
+      const {
+        user
+      } = this.state
+
+      <div>
+        
+      </div>
+    )
+  }
+}
