9c9a326b2f2d1b275119befb18b30f5726022d19
4000d <dkdkajej@gmail.com>
2017-01-23 19:29:10 +0900

    let delete qna, qna.comment

--- src/express/controllers/auth/auth.fetch.qna.js
+++ src/express/controllers/auth/auth.fetch.qna.js
@@ -24,6 +24,7 @@ import * as mh from '../../lib/modelHelper'
 import isLoggedIn from '../../lib/login-check'
 import * as renderHelper from '../../lib/renderHelper'
 import * as renderUser from '../../lib/renderUser'
+import * as ac from '../../lib/auth-check'
 
 const router = express.Router();
 
@@ -42,5 +43,43 @@ router.post('/:_id/comment', isLoggedIn, async (req, res) => {
     res.status(400).json({ error: e.message })
   }
 })
+
+router.delete('/:_id', isLoggedIn, async (req, res) => {
+  try {
+    let user = req.user
+
+    let qna = await QnAModel.findById(req.params._id).populate("project product")
+    if (!qna) throw Error('No qna found')
+
+    if (!ac.canEdit(user, qna.project || qna.product)) throw Error(`Can't delete unauthorized post`)
+
+    let r = await QnAModel.remove({_id: qna._id})
+
+    res.json({ response: r })
+  } catch (e) {
+    res.status(400).json({ error: e.message })
+  }
+})
+
+router.delete('/:_id/comment/:comment_index', isLoggedIn, async (req, res) => {
+  try {
+    let user = req.user
+
+    let qna = await QnAModel.findById(req.params._id).populate("project product")
+    if (!qna) throw Error('No post found')
+
+    let comment = qna.comments[req.params.comment_index]
+    if (!ac.canEditComment(user, qna.product || qna.project, comment)) throw Error(`Can't delete unauthorized post comment`)
+
+    qna.comments.pop(req.params.comment_index)
+
+    let r = await qna.save()
+
+    res.json({ response: r })
+  } catch (e) {
+    res.status(400).json({ error: e.message })
+  }
+})
+
 
 export default router;
